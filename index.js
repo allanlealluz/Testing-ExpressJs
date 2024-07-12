7ac59419a313384b37c5ebf8f64b2e3c4c06cf02
allanlealluz <allanlealluz@gmail.com>
2024-07-11 21:22:02 -0300

    changes 11/07 2

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 import express from "express";
-import { engine } from "express-handlebars";
+import exphbs from "express-handlebars";
+import sqlite3 from "sqlite3"
+import { open } from 'sqlite'
+const dbPromise = open({
+    filename: 'data.db',
+    driver: sqlite3.Database
+})
 
 const app = express();
-const messages = [
-    "valuessss",
-    "value2"
-];
 app.use(express.urlencoded({extended:false}))
-app.engine("handlebars", engine());
+app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    const db = await dbPromise
+    const message = await db.all("SELECT * FROM Message")
     res.render('home', { messages }); // Passar um objeto contendo as mensagens
 });
+app.post('/message', async (req, res) => {
+    const db = await dbPromise
+    const messageText = req.body.messageText
+    await db.run('INSERT INTO Message (text) VALUES (?);', messageText)
+    res.redirect('/')
+  })
 app.get("/dale",(req,res) => {
     res.render('other')
 })
-app.post("/message",(req,res)=>{
-    messages.push(req.body.content)
-    res.redirect("/")
-})
-
-app.listen(3050, () => {
-    console.log('server is running on port 3050');
-});
+const setup = async () => {
+        const db = await dbPromise
+        await db.migrate()
+        app.listen(8000, () => {
+          console.log('listening on localhost:8000')
+        })
+}
+setup()
